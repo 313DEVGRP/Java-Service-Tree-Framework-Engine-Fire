8ec6e1e23d5c835522c8236dc9352be5e8d0b6f7
imingyu <hikakaka13@naver.com>
2024-02-04 17:40:52 +0900

    feat: [ARMS-277] #comment 일반 집계 요청 optional.of->optaionl.ofnullable 로 변경
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::sevoon0909 ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/elasticsearch/util/query/EsQueryBuilder.java
+++ src/main/java/com/arms/elasticsearch/util/query/EsQueryBuilder.java
@@ -1,35 +1,30 @@
 package com.arms.elasticsearch.util.query;
 
 import com.arms.elasticsearch.util.query.bool.EsBoolQuery;
+import com.arms.elasticsearch.util.query.query_string.QueryString;
 import com.arms.elasticsearch.util.query.sort.SortBy;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.Sort;
-
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class EsQueryBuilder extends EsQuery {
 
 	public EsQueryBuilder bool(EsBoolQuery ... esBoolQuery){
-
 		BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
 		for (EsBoolQuery esQuery : esBoolQuery) {
 			esQuery.boolQueryBuilder(boolQueryBuilder);
 		}
-		put(new ParameterizedTypeReference<>(){},boolQueryBuilder);
-
+		setQuery(new ParameterizedTypeReference<>() {},boolQueryBuilder);
 		return this;
 	}
 
 	public EsQueryBuilder sort(SortBy sortBy){
-		put(new ParameterizedTypeReference<>(){},sortBy.sortQuery());
+		setQuery(new ParameterizedTypeReference<>() {},sortBy);
+		return this;
+	}
+
+	public EsQueryBuilder queryString(QueryString queryString){
+		setQuery(new ParameterizedTypeReference<>() {},queryString);
 		return this;
 	}
 
