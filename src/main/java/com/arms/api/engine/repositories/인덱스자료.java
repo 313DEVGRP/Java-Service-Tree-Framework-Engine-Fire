52f9751c646b9e8baf34e8a14a5cad0ca9a46c5e
hsYang <gudtjr4662@naver.com>
2024-02-21 21:42:20 +0900

    feat: [yhs-6] #comment 지라이슈_인덱스명 수정 -24.02.21 #close #time 1h +review SR @HsYang-hub
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::hsYang ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/engine/repositories/인덱스자료.java
+++ src/main/java/com/arms/api/engine/repositories/인덱스자료.java
@@ -2,7 +2,7 @@ package com.arms.api.engine.repositories;
 
 public final  class 인덱스자료 {
 
-    public static final String 지라이슈_인덱스명 = "jiraissue_version_test";
+    public static final String 지라이슈_인덱스명 = "jiraissue";
 
     public static final String 서버정보_인덱스명 ="serverinfo";
 
