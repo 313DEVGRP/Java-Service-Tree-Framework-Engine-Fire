e11539a5c8bb30e5205d28d04e1998735eb96525
Advanc2d <phm940916@gmail.com>
2024-03-04 17:16:25 +0900

    🙈 feat : [PHM-118] #comment 레드마인 온프레미스 우선순위 전략 추가 - 24.03.04
    1. 우선순위 목록 조회
    
    개선사항
    1. 페이징 처리?에 관한 라이브러리 소스 확인
    2. 패키징 정리 필요
    3. 이슈, 상태, 유형 등 추가필요
    
    #close #time 1h +review SR @313devops
    
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::Advanc2d ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/arms/api/jira/jirapriority/strategy/레드마인_온프레미스_지라이슈우선순위_전략.java
@@ -0,0 +1,69 @@
+package com.arms.api.jira.jirapriority.strategy;
+
+import com.arms.api.jira.jirapriority.model.지라이슈우선순위_데이터;
+import com.arms.api.jira.utils.지라API_정보;
+import com.arms.api.serverinfo.model.서버정보_데이터;
+import com.arms.api.serverinfo.service.서버정보_서비스;
+import com.arms.errors.codes.에러코드;
+import com.arms.utils.지라유틸;
+import com.taskadapter.redmineapi.RedmineManager;
+import com.taskadapter.redmineapi.bean.IssuePriority;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.toList;
+
+@Component
+public class 레드마인_온프레미스_지라이슈우선순위_전략 implements 지라이슈우선순위_전략 {
+    private final Logger 로그 = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private 서버정보_서비스 서버정보_서비스;
+
+    @Autowired
+    private 지라유틸 지라유틸;
+
+    @Autowired
+    private 지라API_정보 지라API_정보;
+
+    @Override
+    public List<지라이슈우선순위_데이터> 우선순위_목록_가져오기(Long 연결_아이디) throws Exception {
+        로그.info("레드마인_온프레미스_지라이슈우선순위_전략 "+ 연결_아이디 +" 우선순위_목록_가져오기");
+
+        서버정보_데이터 서버정보 = 서버정보_서비스.서버정보_검증(연결_아이디);
+        RedmineManager 레드마인_매니저 = 지라유틸.레드마인_온프레미스_통신기_생성(서버정보.getUri(), 서버정보.getPasswordOrToken());
+        레드마인_매니저.setObjectsPerPage(지라API_정보.getParameter().getMaxResults());
+        List<지라이슈우선순위_데이터> 지라이슈우선순위_목록 = new ArrayList<>();
+
+        try {
+            List<IssuePriority> 우선순위_목록 = 레드마인_매니저.getIssueManager().getIssuePriorities();
+            지라이슈우선순위_목록 = 우선순위_목록.stream().map(우선순위 -> {
+                        지라이슈우선순위_데이터 지라이슈우선순위_데이터 = 지라이슈우선순위_데이터형_변환(우선순위);
+                        return 지라이슈우선순위_데이터;
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(toList());
+        }
+        catch (Exception e) {
+            로그.error("레드마인 온프레미스 프로젝트 전체 목록 가져오기에 실패하였습니다." +e.getMessage());
+            throw new IllegalArgumentException(에러코드.프로젝트_조회_오류.getErrorMsg());
+        }
+
+        return 지라이슈우선순위_목록;
+    }
+
+    private 지라이슈우선순위_데이터 지라이슈우선순위_데이터형_변환(IssuePriority 우선순위) {
+        지라이슈우선순위_데이터 지라이슈우선순위_데이터 = new 지라이슈우선순위_데이터();
+
+        지라이슈우선순위_데이터.setId(String.valueOf(우선순위.getId()));
+        지라이슈우선순위_데이터.setName(우선순위.getName());
+
+        return 지라이슈우선순위_데이터;
+    }
+}
