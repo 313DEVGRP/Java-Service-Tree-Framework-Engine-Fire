1a56a937f74b0bfdb915ba812baf7d532c8b0358
ssoohyun <chltngus129@gmail.com>
2024-03-09 16:18:47 +0900

    :boom: feat: [Engine] #comment 레드마인 온프레미스 이슈 전략 추가 - 24.03.09 - 이슈 생성하기 추가 #close #time 1h +review SR @313devops
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::ssoohyun ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/jira/jiraissue/strategy/레드마인_온프레미스_이슈_전략.java
+++ src/main/java/com/arms/api/jira/jiraissue/strategy/레드마인_온프레미스_이슈_전략.java
@@ -11,6 +11,9 @@ import com.arms.errors.codes.에러코드;
 import com.arms.utils.지라유틸;
 import com.taskadapter.redmineapi.RedmineManager;
 import com.taskadapter.redmineapi.bean.Issue;
+import com.taskadapter.redmineapi.bean.IssueFactory;
+import com.taskadapter.redmineapi.bean.Tracker;
+import com.taskadapter.redmineapi.bean.TrackerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,11 +52,11 @@ public class 레드마인_온프레미스_이슈_전략 implements 지라이슈_
         RedmineManager 레드마인_매니저 = 지라유틸.레드마인_온프레미스_통신기_생성(서버정보.getUri(), 서버정보.getPasswordOrToken());
 
         레드마인_매니저.setObjectsPerPage(지라API_정보.getParameter().getMaxResults());
-        List<지라이슈_데이터> 지라이슈_목록 = new ArrayList<>();
+        List<지라이슈_데이터> 이슈_목록 = new ArrayList<>();
 
         try {
             List<Issue> 이슈목록 = 레드마인_매니저.getIssueManager().getIssues(프로젝트_키_또는_아이디, null);
-            지라이슈_목록 = 이슈목록.stream().map(이슈 -> {
+            이슈_목록 = 이슈목록.stream().map(이슈 -> {
                         지라이슈_데이터 지라이슈_데이터 = 지라이슈_데이터형_변환(이슈);
                         return 지라이슈_데이터;
                     })
@@ -65,12 +68,39 @@ public class 레드마인_온프레미스_이슈_전략 implements 지라이슈_
             throw new IllegalArgumentException(에러코드.이슈_조회_오류.getErrorMsg());
         }
 
-        return 지라이슈_목록;
+        return 이슈_목록;
     }
 
     @Override
     public 지라이슈_데이터 이슈_생성하기(Long 연결_아이디, 지라이슈생성_데이터 지라이슈생성_데이터) throws Exception {
-        return null;
+
+        로그.info("레드마인_온프레미스_이슈_전략 "+ 연결_아이디 +" 이슈_생성하기");
+
+        서버정보_데이터 서버정보 = 서버정보_서비스.서버정보_검증(연결_아이디);
+        RedmineManager 레드마인_매니저 = 지라유틸.레드마인_온프레미스_통신기_생성(서버정보.getUri(), 서버정보.getPasswordOrToken());
+
+        지라이슈_데이터 이슈_데이터 = null;
+        지라이슈생성필드_데이터 필드_데이터 = 지라이슈생성_데이터.getFields();
+        지라프로젝트_데이터 프로젝트_데이터 = 필드_데이터.getProject();
+        지라이슈유형_데이터 이슈유형_데이터 = 필드_데이터.getIssuetype();
+        지라이슈우선순위_데이터 우선순위_데이터 = 필드_데이터.getPriority();
+
+        try {
+            Issue 생성할_이슈 = IssueFactory.create(Integer.parseInt(프로젝트_데이터.getId()), 필드_데이터.getSummary());
+            Tracker 이슈타입 = TrackerFactory.create(Integer.parseInt(이슈유형_데이터.getId()), 이슈유형_데이터.getName());
+            생성할_이슈.setTracker(이슈타입);
+            생성할_이슈.setDescription(필드_데이터.getDescription());
+            생성할_이슈.setPriorityId(Integer.parseInt(우선순위_데이터.getId()));
+
+            Issue 생성된_이슈 = 레드마인_매니저.getIssueManager().createIssue(생성할_이슈);
+            이슈_데이터 = 지라이슈_데이터형_변환(생성된_이슈);
+        }
+        catch (Exception e) {
+            로그.error("레드마인 온프레미스 이슈 생성하기에 실패하였습니다." +e.getMessage());
+            throw new IllegalArgumentException(에러코드.이슈생성_오류.getErrorMsg());
+        }
+
+        return 이슈_데이터;
     }
 
     @Override
@@ -120,7 +150,7 @@ public class 레드마인_온프레미스_이슈_전략 implements 지라이슈_
 
         지라이슈_데이터.setId(String.valueOf(이슈.getId()));
 
-        지라이슈필드_데이터.setProject(new 지라프로젝트_데이터(String.valueOf(이슈.getId()), 이슈.getProjectName()));
+        지라이슈필드_데이터.setProject(new 지라프로젝트_데이터(String.valueOf(이슈.getProjectId()), 이슈.getProjectName()));
         지라이슈필드_데이터.setIssuetype(new 지라이슈유형_데이터(String.valueOf(이슈.getTracker().getId()), 이슈.getTracker().getName()));
         지라이슈필드_데이터.setPriority(new 지라이슈우선순위_데이터(String.valueOf(이슈.getPriorityId()), 이슈.getPriorityText()));
         지라이슈필드_데이터.setStatus(new 지라이슈상태_데이터(String.valueOf(이슈.getStatusId()), 이슈.getStatusName()));
