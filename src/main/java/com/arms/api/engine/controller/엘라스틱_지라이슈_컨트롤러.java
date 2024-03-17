a2172de85dafd1f084f2e5283e2d98166d515c20
dumbbelloper <dumbbelloper@gmail.com>
2024-03-17 16:05:45 +0900

    fix: [ARMSQA-19] #comment cReqLink 필드 추가로 인해 수동 업데이트 추가
    #close #time 1m +review SR @dumbbelloper
    
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/engine/controller/엘라스틱_지라이슈_컨트롤러.java
+++ src/main/java/com/arms/api/engine/controller/엘라스틱_지라이슈_컨트롤러.java
@@ -53,6 +53,9 @@ public class 엘라스틱_지라이슈_컨트롤러 {
     }
 
 
+    /**
+     * 2024-03-17 사용 여부 체크 필요
+     */
     @ResponseBody
     @RequestMapping(
             value = {"/loadToES/{issueKey}"},
@@ -62,10 +65,11 @@ public class 엘라스틱_지라이슈_컨트롤러 {
                                @PathVariable("issueKey") String 이슈_키,
                                @RequestParam("pdServiceId") Long 제품서비스_아이디,
                                @RequestParam("pdServiceVersions") Long[] 제품서비스_버전_아이디들,
-                                        ModelMap model, HttpServletRequest request) throws Exception {
+                               @RequestParam("cReqLink") Long cReqLink,
+                           ModelMap model, HttpServletRequest request) throws Exception {
         로그.info("지라 이슈_검색엔진_저장");
 
-        return 지라이슈_검색엔진.이슈_검색엔진_저장(지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들);
+        return 지라이슈_검색엔진.이슈_검색엔진_저장(지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들, cReqLink);
     }
 
     @ResponseBody
@@ -99,13 +103,14 @@ public class 엘라스틱_지라이슈_컨트롤러 {
                                        @PathVariable("issueKey") String 이슈_키,
                                        @RequestParam("pdServiceId") Long 제품서비스_아이디,
                                        @RequestParam("pdServiceVersions") Long[] 제품서비스_버전_아이디들,
+                                       @RequestParam("cReqLink") Long cReqLink,
                                        ModelMap model, HttpServletRequest request) throws Exception {
 
         // 로그.info(" [ " + this.getClass().getName() + " :: 이슈_검색엔진_벌크_저장 ]" );
         로그.info("지라서버 아이디 : {}\n이슈 키 : {}\n제품서비스 아이디 : {}\n제품서비스 버전 목록 : {}"
                 , 지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들);
 
-        return 지라이슈_검색엔진.이슈_링크드이슈_서브테스크_벌크로_추가하기(지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들);
+        return 지라이슈_검색엔진.이슈_링크드이슈_서브테스크_벌크로_추가하기(지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들, cReqLink);
     }
 
     @ResponseBody
@@ -117,12 +122,13 @@ public class 엘라스틱_지라이슈_컨트롤러 {
                              @PathVariable("issueKey") String 이슈_키,
                              @RequestParam("pdServiceId") Long 제품서비스_아이디,
                              @RequestParam("pdServiceVersions") Long[] 제품서비스_버전_아이디들,
+                             @RequestParam("cReqLink") Long cReqLink,
                              ModelMap model, HttpServletRequest request) throws Exception {
 
         로그.info("지라서버 아이디 : {},\t이슈 키 : {},\t제품서비스 아이디 : {},\t제품서비스 버전 목록 : {}"
                             , 지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들);
 
-        return 지라이슈_검색엔진.증분이슈_링크드이슈_서브테스크_벌크추가(지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들);
+        return 지라이슈_검색엔진.증분이슈_링크드이슈_서브테스크_벌크추가(지라서버_아이디, 이슈_키, 제품서비스_아이디, 제품서비스_버전_아이디들, cReqLink);
     }
 
     @ResponseBody
