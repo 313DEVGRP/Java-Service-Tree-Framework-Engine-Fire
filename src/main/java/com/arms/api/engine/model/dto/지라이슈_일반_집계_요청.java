7bddc9a1a08c1dd7553888c471adb28130e38922
sevoon0909 <sevoon0909@icloud.com>
2024-03-31 12:05:38 +0900

    refactor : [JIRA-7] #comment 비슷한 요청값들 모음 #close #time 1h +review SR @sevoon0909
    [Engine-Fire::Java-Service-Tree-Framework(JSTF)::mingyulee ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/engine/model/dto/지라이슈_일반_집계_요청.java
+++ src/main/java/com/arms/api/engine/model/dto/지라이슈_일반_집계_요청.java
@@ -1,12 +1,21 @@
 package com.arms.api.engine.model.dto;
 
+import com.arms.api.engine.model.enums.IsReqType;
 import com.arms.elasticsearch.query.base.기본_집계_요청;
 
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class 지라이슈_일반_집계_요청 extends 기본_집계_요청 {
+
+	private IsReqType isReqType;
 	private Boolean isReq;
+	private List<Long> pdServiceVersionLinks;
+
+	private String 필터필드;
+	private List<?> 필터필드검색어;
 }
