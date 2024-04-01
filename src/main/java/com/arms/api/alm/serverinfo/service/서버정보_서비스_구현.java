972512b192d718396e4fdd4cdd39e8c19d508ee4
hsYang <gudtjr4662@naver.com>
2024-04-02 00:11:10 +0900

    feat: [YHS-73] #comment 임포트 설정 변경 -24.04.02 #close #time 1h +review SR @HsYang-hub
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::hsYang ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/alm/serverinfo/service/서버정보_서비스_구현.java
+++ src/main/java/com/arms/api/alm/serverinfo/service/서버정보_서비스_구현.java
@@ -2,6 +2,7 @@ package com.arms.api.alm.serverinfo.service;
 
 import com.arms.api.alm.serverinfo.model.서버정보_데이터;
 import com.arms.api.alm.issue.repository.지라이슈_저장소;
+import com.arms.api.alm.serverinfo.repository.서버정보_저장소;
 import com.arms.api.index_entity.서버정보_인덱스;
 import com.arms.api.utils.errors.codes.에러코드;
 
@@ -32,7 +33,7 @@ public class 서버정보_서비스_구현 implements 서버정보_서비스 {
     private ModelMapper modelMapper;
 
     @Autowired
-    private com.arms.api.alm.serverinfo.repository.서버정보_저장소 서버정보_저장소;
+    private 서버정보_저장소 서버정보_저장소;
 
     @Autowired
     private 백엔드통신기 백엔드통신기;
