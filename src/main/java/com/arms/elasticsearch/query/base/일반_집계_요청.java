c28402392c841b46abba7db30404b9d21d219c5b
hsYang <gudtjr4662@naver.com>
2024-05-11 17:34:39 +0900

    feat: [YHS-122] #comment 집계결과 정렬 기본설정 내림차순으로 변경. -24.05.11 #close #time 1h +review SR @HsYang-hub
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::hsYang ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/elasticsearch/query/base/일반_집계_요청.java
+++ src/main/java/com/arms/elasticsearch/query/base/일반_집계_요청.java
@@ -11,6 +11,6 @@ public abstract class 일반_집계_요청 extends 기본_검색_요청 {
 	private String 메인그룹필드;
 	private boolean 컨텐츠보기여부;
 	private int 하위크기 = 1000;
-	private boolean 결과_갯수_기준_오름차순 = true;
+	private boolean 결과_갯수_기준_오름차순 = false;
 
 }
