7ca32768d8dba148fd8e16dee2f033dc5bc6f3be
hsYang <gudtjr4662@naver.com>
2024-05-29 23:07:00 +0900

    feat: [YHS-161] #comment 제품_버전_요구사항 관련 집계 API 추가 -24.05.29 #close #time 1h +review SR @HsYang-hub
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::hsYang ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/alm/dashboard/controller/지라이슈_대시보드_컨트롤러.java
+++ src/main/java/com/arms/api/alm/dashboard/controller/지라이슈_대시보드_컨트롤러.java
@@ -5,6 +5,7 @@ import com.arms.api.util.model.dto.트리맵_검색__집계_하위_요청;
 import com.arms.api.util.model.vo.Worker;
 import com.arms.egovframework.javaservice.esframework.EsQuery;
 import com.arms.egovframework.javaservice.esframework.esquery.EsQueryBuilder;
+import com.arms.egovframework.javaservice.esframework.filter.TermsQueryFilter;
 import com.arms.egovframework.javaservice.esframework.must.TermQueryMust;
 import com.arms.egovframework.javaservice.esframework.factory.creator.중첩_집계_쿼리_생성기;
 import com.arms.egovframework.javaservice.esframework.model.vo.버킷_집계_결과_목록_합계;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.arms.api.alm.dashboard.service.지라이슈_대시보드_서비스;
 
 import java.util.List;
 
@@ -21,7 +23,7 @@ import java.util.List;
 public class 지라이슈_대시보드_컨트롤러 {
 
     @Autowired
-    private com.arms.api.alm.dashboard.service.지라이슈_대시보드_서비스 지라이슈_대시보드_서비스;
+    private 지라이슈_대시보드_서비스 지라이슈_대시보드_서비스;
 
 
     // Dashboard
@@ -58,4 +60,18 @@ public class 지라이슈_대시보드_컨트롤러 {
         return ResponseEntity.ok(지라이슈_대시보드_서비스.집계결과_가져오기(중첩_집계_쿼리_생성기.포괄(지라이슈_일반_집계_요청, esQuery)));
     }
 
+    @GetMapping("/pdService-version-req/{pdServiceId}")
+    public ResponseEntity<버킷_집계_결과_목록_합계> 제품_버전_요구사항_이슈_집계(@PathVariable Long pdServiceId,
+                                                     @RequestParam List<Long> pdServiceVersionLinks,
+                                                     지라이슈_기본_검색__집계_하위_요청 지라이슈_일반_집계_요청) {
+        EsQuery esQuery
+                = new EsQueryBuilder()
+                .bool(
+                        new TermQueryMust("pdServiceId", pdServiceId),
+                        new TermsQueryFilter("pdServiceVersions", pdServiceVersionLinks),
+                        new TermQueryMust("isReq", 지라이슈_일반_집계_요청.getIsReq())
+                );
+
+        return ResponseEntity.ok(지라이슈_대시보드_서비스.집계결과_가져오기(중첩_집계_쿼리_생성기.포괄(지라이슈_일반_집계_요청, esQuery)));
+    }
 }
