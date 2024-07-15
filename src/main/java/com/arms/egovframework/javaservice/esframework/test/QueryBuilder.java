7aaae9ff5c617ab54ba97eec0003c7b31ce9c191
sevoon0909 <sevoon0909@icloud.com>
2024-07-16 01:21:45 +0900

    fix: [ARMSQA-19] #comment es dsl 만들기 #close #time 1h +review SR @sevoon0909
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::sevoon0909 ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/egovframework/javaservice/esframework/test/QueryBuilder.java
+++ src/main/java/com/arms/egovframework/javaservice/esframework/test/QueryBuilder.java
@@ -1,60 +1,44 @@
 package com.arms.egovframework.javaservice.esframework.test;
 
-import java.util.Arrays;
+
+
+import static com.arms.egovframework.javaservice.esframework.test.FilterBuilder.*;
+import static com.arms.egovframework.javaservice.esframework.test.AggregationBuilder.*;
+
 import java.util.function.Consumer;
+
+import org.checkerframework.checker.units.qual.A;
 
 public class QueryBuilder {
 
-	public static Filter filter(TermBuilderAbstract... termBuilderAbstracts){
 
-		Filter filter = new Filter();
-
-		Arrays.stream(termBuilderAbstracts).forEach(
-			builders->{
-				if(builders instanceof TermBuilder){
-					filter.addFilter(((TermBuilder)builders).term());
-				}else if(builders instanceof TermsBuilder){
-					filter.addFilter(((TermsBuilder)builders).terms());
-				}
-			}
-		);
-
-		return filter;
+	public QueryBuilder query(Filter... filter){
+		return this;
 	}
 
-	public static TermBuilderAbstract terms(Consumer<TermsBuilder> consumers) {
-		return buildTerms(consumers);
-	}
-
-	private static TermsBuilder buildTerms( Consumer<TermsBuilder> consumer) {
-		TermsBuilder termsBuilder = new TermsBuilder();
-		consumer.accept(termsBuilder);
-		return termsBuilder;
-	}
-
-	public static TermBuilderAbstract term(Consumer<TermBuilder> consumers) {
-		return buildTerm(consumers);
-	}
-
-	private static TermBuilder buildTerm( Consumer<TermBuilder> consumer) {
-		TermBuilder termBuilder = new TermBuilder();
-		consumer.accept(termBuilder);
-		return termBuilder;
+	public QueryBuilder aggregation(AggregationTermsBuilder... aggregationTermsBuilders){
+		return this;
 	}
 
 	public static void main(String[] args) {
-		Filter filter = QueryBuilder.filter(
-			term(
-				a -> a.field("순대").value("떡볶이")
-			),
-			term(
-				a -> a.field("김치").value("볶음밥")
-			),
-			terms(
-				a -> a.field("김치").value("볶음밥","라면")
+
+		QueryBuilder aggregation = new QueryBuilder().query(
+			filter(
+				term(
+					a -> a.field("순대").value("떡볶이")
+				),
+				term(
+					a -> a.field("김치").value("볶음밥")
+				),
+				terms(
+					a -> a.field("김치").value("볶음밥", "라면")
+				)
+			)
+		).aggregation(
+			aggrTerms(
+				a -> a.name("그룹")
 			)
 		);
-		System.out.println(filter);
 
 	}
 
