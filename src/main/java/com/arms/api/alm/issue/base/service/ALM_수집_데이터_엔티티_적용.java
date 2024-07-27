425ebc26a658db1e1397fb6e4142c05c15b336b0
imingyu <hikakaka13@naver.com>
2024-07-27 17:51:28 +0900

    refactor: [KAN-99] #comment 지라이슈 벌크 추가 메소드 최적화- 24.07.27 기능별로 분리 진행 #close #time 1h +review SR @sevoon0909
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [GitLab] http://www.313.co.kr/gitlab
    [Redmine] http://www.313.co.kr:3000/
    
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    
    [Docker Hub] https://hub.docker.com/u/313devgrp
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    [Kafka] http://www.313.co.kr/kafka
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio
    [DrawDB] http://www.313.co.kr/drawdb

---
+++ src/main/java/com/arms/api/alm/issue/base/service/ALM_수집_데이터_엔티티_적용.java
@@ -0,0 +1,131 @@
+package com.arms.api.alm.issue.base.service;
+
+import com.arms.api.alm.issue.base.model.dto.지라이슈_데이터;
+import com.arms.api.alm.issue.base.model.dto.지라이슈_엔티티;
+import com.arms.api.alm.issue.base.model.vo.지라이슈_벌크_추가_요청;
+import com.arms.api.alm.utils.지라이슈_생성;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.arms.config.ApplicationContextProvider.getBean;
+
+
+public class ALM_수집_데이터_엔티티_적용 {
+
+    private final 지라이슈_엔티티_컬렉션 지라이슈_엔티티_저장_목록 = new 지라이슈_엔티티_컬렉션(new ArrayList<>());
+
+    private final List<지라이슈_엔티티> 지라이슈_엔티티_요구사항_하위이슈_목록;
+
+    private final List<지라이슈_데이터> 지라이슈_데이터_하위이슈_목록;
+
+    @Getter
+    private final 지라이슈_데이터 이슈_상세정보_가져오기;
+
+    private final 지라이슈_벌크_추가_요청 지라이슈_벌크_추가_요청값;
+
+
+
+    public List<지라이슈_엔티티> 삭제할_지라이슈_엔티티_목록(){
+        return 지라이슈_엔티티_저장_목록.get지라이슈_엔티티_목록();
+    }
+
+    public ALM_수집_데이터_엔티티_적용(지라이슈_벌크_추가_요청 지라이슈_벌크_추가_요청값){
+
+        this.지라이슈_벌크_추가_요청값 = 지라이슈_벌크_추가_요청값;
+
+        this.지라이슈_엔티티_요구사항_하위이슈_목록 = getBean(서브테스크_조회.class).요구사항_서브테스크_검색하기(지라이슈_벌크_추가_요청값);
+
+        this.지라이슈_데이터_하위이슈_목록 = getBean(이슈전략_호출.class).서브테스크_가져오기(지라이슈_벌크_추가_요청값);
+
+        this.이슈_상세정보_가져오기 = getBean(이슈전략_호출.class).이슈_상세정보_가져오기(지라이슈_벌크_추가_요청값);
+
+    }
+
+    public 지라이슈_엔티티 지라이슈_삭제_적용(지라이슈_엔티티 지라이슈_엔티티값){
+
+        String 이슈_삭제_년월일 = LocalDate.now()
+            .minusDays(1)
+            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        if(지라이슈_엔티티값!=null&&지라이슈_엔티티값.izNotEmpty()){
+            지라이슈_엔티티.삭제 삭제데이터 = 지라이슈_엔티티값.getDeleted();
+            if (삭제데이터 == null) {
+                삭제데이터 = new 지라이슈_엔티티.삭제();
+            }
+            삭제데이터.setDeleted_date(이슈_삭제_년월일);
+            삭제데이터.setIsDeleted(true);
+            지라이슈_엔티티값.setDeleted(삭제데이터);
+        }
+
+        return 지라이슈_엔티티값;
+
+    }
+
+    private void 지라이슈_요구사항_연결_끊기_적용(지라이슈_엔티티 지라이슈_엔티티값){
+        지라이슈_엔티티값.setParentReqKey(null);
+    }
+
+    public void 지라이슈_요구사항_연결_끊고_삭제_적용(지라이슈_엔티티 지라이슈_엔티티값){
+        지라이슈_요구사항_연결_끊기_적용(지라이슈_엔티티값);
+        지라이슈_엔티티_저장_목록.엔티티_목록_추가(지라이슈_삭제_적용(지라이슈_엔티티값));
+    }
+
+    public void 지라이슈_일괄_삭제_적용(){
+        지라이슈_엔티티_요구사항_하위이슈_목록.forEach(
+            this::지라이슈_삭제_적용
+        );
+        지라이슈_엔티티_저장_목록.엔티티_목록_리스트_전체_추가(지라이슈_엔티티_요구사항_하위이슈_목록);
+    }
+
+    private void 지라이슈_일괄_삭제_적용(List<지라이슈_엔티티> 요구사항_서브테스크_삭제_목록값){
+        요구사항_서브테스크_삭제_목록값.forEach(
+            this::지라이슈_삭제_적용
+        );
+        지라이슈_엔티티_저장_목록.엔티티_목록_리스트_전체_추가(요구사항_서브테스크_삭제_목록값);
+    }
+
+    public void 지라이슈_데이터에_존재하지_않는_지라이슈_엔티티_삭제_적용(){
+        지라이슈_데이터_컬렉션 지라이슈_데이터_컬렉션 = new 지라이슈_데이터_컬렉션(지라이슈_데이터_하위이슈_목록);
+        지라이슈_엔티티_컬렉션 지라이슈_엔티티_컬렉션 = new 지라이슈_엔티티_컬렉션(지라이슈_엔티티_요구사항_하위이슈_목록);
+        this.지라이슈_일괄_삭제_적용(
+            지라이슈_데이터_컬렉션.지라이슈_데이터에_존재하지_않는_지라이슈_목록(지라이슈_엔티티_컬렉션)
+                .get지라이슈_엔티티_목록()
+        );
+    }
+
+    public void 지라이슈_엔티티_요구사항_적용() {
+
+            this.지라이슈_엔티티_저장_목록.엔티티_목록_추가(지라이슈_생성.ELK_데이터로_변환(
+                    지라이슈_벌크_추가_요청값.get지라서버_아이디()
+                    , 이슈_상세정보_가져오기
+                    , true
+                    , null
+                    , 지라이슈_벌크_추가_요청값.get제품서비스_아이디()
+                    , 지라이슈_벌크_추가_요청값.get제품서비스_버전들()
+                    , 지라이슈_벌크_추가_요청값.getCReqLink()
+            )
+        );
+
+    }
+
+    public void 지라이슈_엔티티_하위이슈_적용() {
+
+        this.지라이슈_데이터_하위이슈_목록
+            .forEach(ALM서브테스크 -> {
+                지라이슈_엔티티 변환된_이슈 =
+                        지라이슈_생성.ELK_데이터로_변환(
+                            지라이슈_벌크_추가_요청값.get지라서버_아이디()
+                            , ALM서브테스크
+                            ,false
+                            , 지라이슈_벌크_추가_요청값.get이슈_키()
+                            , 지라이슈_벌크_추가_요청값.get제품서비스_아이디()
+                            , 지라이슈_벌크_추가_요청값.get제품서비스_버전들()
+                            , 지라이슈_벌크_추가_요청값.getCReqLink()
+                        );
+                지라이슈_엔티티_저장_목록.엔티티_목록_추가(변환된_이슈);
+            });
+    }
+}
