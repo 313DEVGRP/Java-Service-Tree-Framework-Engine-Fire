3ba1d64002749180b183c8dc54aa532e921b1765
sevoon0909 <sevoon0909@icloud.com>
2024-08-04 15:52:48 +0900

    refactor: [Engine] #comment 리팩토링 - 24.08.04
    #close #time 1h +review SR @mklee
    
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::mklee ] [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/alm/issue/base/service/jiraissue_schedule/alm_entity_sync/ALM_수집_데이터_지라이슈_엔티티_동기화.java
+++ src/main/java/com/arms/api/alm/issue/base/service/jiraissue_schedule/alm_entity_sync/ALM_수집_데이터_지라이슈_엔티티_동기화.java
@@ -176,7 +176,7 @@ public class ALM_수집_데이터_지라이슈_엔티티_동기화 {
 
         Set<String> 연결이슈_아이디_세트 = new HashSet<>();
 
-        이슈링크_가져오기_재귀(요청값, 요청값.get이슈_키(), 연결이슈_아이디_세트);
+        이슈링크_가져오기(요청값, 요청값.get이슈_키(), 연결이슈_아이디_세트);
 
         if (연결이슈_아이디_세트.isEmpty()) {
             return new String[0];
@@ -185,23 +185,15 @@ public class ALM_수집_데이터_지라이슈_엔티티_동기화 {
         }
     }
 
-    private void 이슈링크_가져오기_재귀(지라이슈_벌크_추가_요청 요청값, String 현재_이슈_키, Set<String> 연결이슈_아이디_세트) {
+    private void 이슈링크_가져오기(지라이슈_벌크_추가_요청 요청값, String 현재_이슈_키, Set<String> 연결이슈_아이디_세트) {
 
         List<지라이슈_데이터> 이슈링크_가져오기 =
-                Optional.ofNullable(ALM_이슈링크_가져오기(요청값.get지라서버_아이디(),요청값.get이슈_키()))
-                        .orElse(Collections.emptyList());
-        if(!이슈링크_가져오기.isEmpty()){
+            Optional.ofNullable(ALM_이슈링크_가져오기(요청값.get지라서버_아이디(),요청값.get이슈_키()))
+                .orElse(Collections.emptyList());
+        if (!이슈링크_가져오기.isEmpty()) {
             for (지라이슈_데이터 ALM_데이터 : 이슈링크_가져오기) {
                 String 조회조건_아이디 = 요청값.get지라서버_아이디()+"_"+ALM_데이터.getFields().getProject().getKey()+"_"+ALM_데이터.getKey();
-
-                if (ALM_데이터.getKey().equals(요청값.get이슈_키()) || 연결이슈_아이디_세트.contains(조회조건_아이디)) {
-                    continue;
-                } else {
-                    연결이슈_아이디_세트.add(조회조건_아이디);
-                }
-
-                // 내부 연결 이슈를 재귀적으로 가져오기
-                이슈링크_가져오기_재귀(요청값, ALM_데이터.getKey(), 연결이슈_아이디_세트);
+                연결이슈_아이디_세트.add(조회조건_아이디);
             }
         }
     }
