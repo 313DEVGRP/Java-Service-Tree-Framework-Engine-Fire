02790351c12983fc6c37dd2672f8c84ccf70d0d1
sevoon0909 <sevoon0909@icloud.com>
2024-08-04 18:50:28 +0900

    refactor: [Engine] #comment 리팩토링 요구사항이슈_처리 클래스 별도로 추가 하여 해당 클래스에서 연결이슈_처리 호출 하게 변경  - 24.08.04
    #close #time 1h +review SR @mklee
    
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::mklee ] [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/api/alm/issue/base/service/jiraissue_schedule/alm_entity_sync/ALM_수집_데이터_지라이슈_엔티티_동기화.java
+++ src/main/java/com/arms/api/alm/issue/base/service/jiraissue_schedule/alm_entity_sync/ALM_수집_데이터_지라이슈_엔티티_동기화.java
@@ -8,22 +8,19 @@ import java.util.List;
 import com.arms.api.alm.issue.base.model.dto.지라이슈_데이터;
 import com.arms.api.alm.issue.base.model.dto.지라이슈_엔티티;
 import com.arms.api.alm.issue.base.model.vo.지라이슈_벌크_추가_요청;
-import com.arms.api.alm.issue.base.service.jiraissue_schedule.alm_entity_sync.ALM_수집_데이터_지라이슈_엔티티_삭제_처리;
-import com.arms.api.alm.issue.base.service.jiraissue_schedule.alm_entity_sync.연결이슈_처리;
-import com.arms.api.alm.issue.base.service.jiraissue_schedule.alm_entity_sync.지라이슈_엔티티_컬렉션;
 import com.arms.api.alm.issue.base.service.jiraissue_schedule.alm_streategy.이슈전략_호출;
 
 public class ALM_수집_데이터_지라이슈_엔티티_동기화 {
 
 	private final 지라이슈_데이터 지라이슈_데이터;
-	private final com.arms.api.alm.issue.base.service.jiraissue_schedule.alm_entity_sync.연결이슈_처리 연결이슈_처리;
+	private final 요구사항이슈_처리 요구사항이슈_처리;
 	private final com.arms.api.alm.issue.base.service.jiraissue_schedule.alm_entity_sync.ALM_수집_데이터_지라이슈_엔티티_동기화_인터페이스 ALM_수집_데이터_지라이슈_엔티티_동기화_인터페이스;
 
 	public ALM_수집_데이터_지라이슈_엔티티_동기화(지라이슈_벌크_추가_요청 지라이슈_벌크_추가_요청값) {
 
 		this.지라이슈_데이터 = getBean(이슈전략_호출.class).이슈_상세정보_가져오기(지라이슈_벌크_추가_요청값);
 
-		this.연결이슈_처리 = new 연결이슈_처리(지라이슈_데이터,지라이슈_벌크_추가_요청값);
+		this.요구사항이슈_처리 = new 요구사항이슈_처리(지라이슈_데이터,지라이슈_벌크_추가_요청값);
 
 		if(가져온_ALM_이슈_없음()){
 			this.ALM_수집_데이터_지라이슈_엔티티_동기화_인터페이스 = new ALM_수집_데이터_지라이슈_엔티티_삭제_처리(지라이슈_벌크_추가_요청값);
@@ -39,7 +36,7 @@ public class ALM_수집_데이터_지라이슈_엔티티_동기화 {
 			return ALM_수집_데이터_지라이슈_엔티티_동기화_인터페이스.지라이슈_앤티티_저장할_목록_가져오기();
 		}else{
 			지라이슈_엔티티_컬렉션 지라이슈_엔티티_컬렉션 = new 지라이슈_엔티티_컬렉션(ALM_수집_데이터_지라이슈_엔티티_동기화_인터페이스.지라이슈_앤티티_저장할_목록_가져오기());
-			지라이슈_엔티티_컬렉션.엔티티_목록_추가(this.연결이슈_처리.요구사항_이슈());
+			지라이슈_엔티티_컬렉션.엔티티_목록_추가(this.요구사항이슈_처리.요구사항_이슈());
 			return 지라이슈_엔티티_컬렉션.get지라이슈_엔티티_목록();
 		}
 	}
