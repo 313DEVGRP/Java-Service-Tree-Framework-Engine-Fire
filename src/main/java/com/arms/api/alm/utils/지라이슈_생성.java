234f439d89393fb2d3a45d77b9fcec4103e46f12
mingyu09 <mklee@mz.co.kr>
2024-08-05 15:44:30 +0900

    refactor: [KAN-99] #comment 리팩토링 - 24.08.05 변경이 필요 한 부분만 if문 처리
    #close #time 1h +review SR @sevoon0909
    
    추신 : 맨앞에 붙일 수 있는 구분
    •fix : Bug 류 처리 시 ( patch version up )
    •feat : 신규 기능 류 처리 시 ( minor version up )
    •perf : 메이저 기능 류 처리 시 ( major version up )
    
    코드리뷰는 코드를 통해 소통하고, 서로의 경험을 통해 배우면서 성장 할 수 있는 기회입니다.
    나를 위해, 나의 동료를 위해, 우리 팀을 위해 리뷰 시간을 사용해 주세요.
    •너무 많은 코드를 한꺼번에 리뷰하지 마세요.
    •최소한의 설명과 함께 중점적으로 봐야 할 곳을 알려 주세요.
    •리뷰 후 Comment를 남겨 주세요.
    •리뷰는 빠른 시간에 응답해 주세요.
    •지적 보다는 대안을 제시해 주세요.
    
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [GitLab] http://www.313.co.kr/gitlab
    [Redmine] http://www.313.co.kr:3000/
    
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    
    [Docker Hub] https://hub.docker.com/u/313devgrp
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    [Kafka] http://www.313.co.kr/kafka
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/
    [DrawIO] http://www.313.co.kr/drawio
    [DrawDB] http://www.313.co.kr/drawdb

--- src/main/java/com/arms/api/alm/utils/지라이슈_생성.java
+++ src/main/java/com/arms/api/alm/utils/지라이슈_생성.java
@@ -18,141 +18,7 @@ public class 지라이슈_생성 {
             Long cReqLink
     ) {
 
-        지라이슈_엔티티.프로젝트 프로젝트 = Optional.ofNullable(지라이슈_데이터.getFields().getProject())
-                .map(project -> 지라이슈_엔티티.프로젝트.builder()
-                        .id(project.getId())
-                        .key(project.getKey())
-                        .name(project.getName())
-                        .self(project.getSelf())
-                        .build())
-                .orElse(null);
-
-        지라이슈_엔티티.이슈유형 이슈유형 = Optional.ofNullable(지라이슈_데이터.getFields().getIssuetype())
-                .map(issuetype -> 지라이슈_엔티티.이슈유형.builder()
-                        .self(issuetype.getSelf())
-                        .id(issuetype.getId())
-                        .description(issuetype.getDescription())
-                        .name(issuetype.getName())
-                        .subtask(issuetype.getSubtask())
-                        .untranslatedName(issuetype.getUntranslatedName())
-                        .hierarchyLevel(issuetype.getHierarchyLevel())
-                        .build())
-                .orElse(null);
-
-        지라이슈_엔티티.생성자 생성자 = Optional.ofNullable(지라이슈_데이터.getFields().getCreator())
-                .map(creator -> 지라이슈_엔티티.생성자.builder()
-                        .accountId(creator.getAccountId())
-                        .emailAddress(creator.getEmailAddress())
-                        .displayName(creator.getDisplayName())
-                        .build())
-                .orElse(null);
-
-        지라이슈_엔티티.보고자 보고자 = Optional.ofNullable(지라이슈_데이터.getFields().getReporter())
-                .map(reporter -> 지라이슈_엔티티.보고자.builder()
-                        .accountId(reporter.getAccountId())
-                        .emailAddress(reporter.getEmailAddress())
-                        .displayName(reporter.getDisplayName())
-                        .build())
-                .orElse(null);
-
-        지라이슈_엔티티.담당자 담당자 = Optional.ofNullable(지라이슈_데이터.getFields().getAssignee())
-                .map(assignee -> 지라이슈_엔티티.담당자.builder()
-                        .accountId(assignee.getAccountId())
-                        .emailAddress(assignee.getEmailAddress())
-                        .displayName(assignee.getDisplayName())
-                        .build())
-                .orElse(null);
-
-        지라이슈_엔티티.우선순위 우선순위 = Optional.ofNullable(지라이슈_데이터.getFields().getPriority())
-                .map(priority -> 지라이슈_엔티티.우선순위.builder()
-                        .self(priority.getSelf())
-                        .id(priority.getId())
-                        .name(priority.getName())
-                        .description(priority.getDescription())
-                        .isDefault(Optional.of(priority.isDefault()).orElse(false)) // 기본값을 false로 설정
-                        .build())
-                .orElse(null);
-
-        지라이슈_엔티티.상태 상태 = Optional.ofNullable(지라이슈_데이터.getFields().getStatus())
-                .map(status -> 지라이슈_엔티티.상태.builder()
-                        .self(status.getSelf())
-                        .id(status.getId())
-                        .name(status.getName())
-                        .description(status.getDescription())
-                        .build())
-                .orElse(null);
-
-        지라이슈_엔티티.해결책 해결책 = Optional.ofNullable(지라이슈_데이터.getFields().getResolution())
-                .map(resolution -> 지라이슈_엔티티.해결책.builder()
-                        .self(resolution.getSelf())
-                        .id(resolution.getId())
-                        .name(resolution.getName())
-                        .description(resolution.getDescription())
-                        .isDefault(Optional.of(resolution.isDefault()).orElse(false)) // 기본값을 false로 설정
-                        .build())
-                .orElse(null);
-
-        List<지라이슈_엔티티.워크로그> 워크로그 = Optional.ofNullable(지라이슈_데이터.getFields().getWorklogs())
-                .orElse(Collections.emptyList()) // null인 경우 빈 리스트 반환
-                .stream()
-                .map(워크로그아이템 -> {
-                    지라이슈_엔티티.저자 저자 = Optional.ofNullable(워크로그아이템.getAuthor())
-                            .map(author -> new 지라이슈_엔티티.저자(
-                                    author.getAccountId(),
-                                    author.getEmailAddress()))
-                            .orElse(null);
-
-                    지라이슈_엔티티.수정한_저자 수정한_저자 = Optional.ofNullable(워크로그아이템.getUpdateAuthor())
-                            .map(updateAuthor -> new 지라이슈_엔티티.수정한_저자(
-                                    updateAuthor.getAccountId(),
-                                    updateAuthor.getEmailAddress()))
-                            .orElse(null);
-
-                    return new 지라이슈_엔티티.워크로그(워크로그아이템.getSelf(),
-                            저자,
-                            수정한_저자,
-                            워크로그아이템.getCreated(),
-                            워크로그아이템.getUpdated(),
-                            워크로그아이템.getStarted(),
-                            워크로그아이템.getTimeSpent(),
-                            워크로그아이템.getTimeSpentSeconds(),
-                            워크로그아이템.getId(),
-                            워크로그아이템.getIssueId()
-                    );
-                })
-                .collect(toList());
-
-        지라이슈_엔티티 이슈 = 지라이슈_엔티티.builder()
-                .jira_server_id(지라서버_아이디)
-                .issueID(지라이슈_데이터.getId())
-                .key(지라이슈_데이터.getKey())
-                .self(지라이슈_데이터.getSelf())
-                .parentReqKey(부모_요구사항_키)
-                .upperKey(지라이슈_데이터.getUpperKey())
-                .isReq(요구사항유형_여부)
-                .project(프로젝트)
-                .issuetype(이슈유형)
-                .creator(생성자)
-                .reporter(보고자)
-                .assignee(담당자)
-                .labels(지라이슈_데이터.getFields().getLabels())
-                .priority(우선순위)
-                .status(상태)
-                .resolution(해결책)
-                .resolutiondate(지라이슈_데이터.getFields().getResolutiondate())
-                .created(지라이슈_데이터.getFields().getCreated())
-                .updated(지라이슈_데이터.getFields().getUpdated())
-                .worklogs(워크로그)
-                .timespent(지라이슈_데이터.getFields().getTimespent())
-                .summary(지라이슈_데이터.getFields().getSummary())
-                .pdServiceId(제품서비스_아이디)
-                .pdServiceVersions(제품서비스_버전들)
-                .cReqLink(cReqLink)
-                .build();
-
-        이슈.generateId();
-
-        return 이슈;
+        return get지라이슈_엔티티(지라서버_아이디, 지라이슈_데이터, 요구사항유형_여부, 부모_요구사항_키, 제품서비스_아이디, 제품서비스_버전들, cReqLink,null);
     }
 
     public static 지라이슈_엔티티 ELK_데이터로_변환(
@@ -161,8 +27,19 @@ public class 지라이슈_생성 {
             Long 제품서비스_아이디, Long[] 제품서비스_버전들,
             Long cReqLink, String[] 연결이슈_아이디들
     ) {
+        return get지라이슈_엔티티(지라서버_아이디, 지라이슈_데이터, 요구사항유형_여부, 부모_요구사항_키, 제품서비스_아이디, 제품서비스_버전들, cReqLink,연결이슈_아이디들);
+    }
 
-
+    private static 지라이슈_엔티티 get지라이슈_엔티티(
+            Long 지라서버_아이디
+            , 지라이슈_데이터 지라이슈_데이터
+            , Boolean 요구사항유형_여부
+            , String 부모_요구사항_키
+            , Long 제품서비스_아이디
+            , Long[] 제품서비스_버전들
+            , Long cReqLink
+            , String[] 연결이슈_아이디들
+    ) {
         지라이슈_엔티티.프로젝트 프로젝트 = Optional.ofNullable(지라이슈_데이터.getFields().getProject())
                 .map(project -> 지라이슈_엔티티.프로젝트.builder()
                         .id(project.getId())
@@ -267,7 +144,7 @@ public class 지라이슈_생성 {
                 })
                 .collect(toList());
 
-        지라이슈_엔티티 이슈 = 지라이슈_엔티티.builder()
+        지라이슈_엔티티.지라이슈_엔티티Builder 지라이슈_엔티티Builder = 지라이슈_엔티티.builder()
                 .jira_server_id(지라서버_아이디)
                 .issueID(지라이슈_데이터.getId())
                 .key(지라이슈_데이터.getKey())
@@ -292,12 +169,17 @@ public class 지라이슈_생성 {
                 .summary(지라이슈_데이터.getFields().getSummary())
                 .pdServiceId(제품서비스_아이디)
                 .pdServiceVersions(제품서비스_버전들)
-                .cReqLink(cReqLink)
-                .linkedIssues(연결이슈_아이디들)
-                .build();
+                .cReqLink(cReqLink);
+
+        if(연결이슈_아이디들!=null){
+            지라이슈_엔티티Builder.linkedIssues(연결이슈_아이디들);
+        }
+
+        지라이슈_엔티티 이슈 = 지라이슈_엔티티Builder.build();
 
         이슈.generateId();
 
         return 이슈;
     }
+
 }
